214cb7377d6297181dd27aa20a9db5549faafd69
Vishal Prajapati <vishalp951295@gmail.com>
2024-08-20 09:13:34 +0530

    Add project files.

---
+++ ducktask/duck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ducktask
+{
+    public class duck : myduck , superduck
+    {
+        public int a;
+       public void display()
+        {
+            Console.WriteLine("enter 1 for finding your duck");
+            Console.WriteLine("enter 2 for finding your toy duck");
+              a =Convert.ToInt32(Console.ReadLine());
+
+            if (a == 1)
+            {
+                
+
+                
+            }
+        }
+    }
+}
---
+++ ducktask/myduck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ducktask
+{
+    public class myduck : superduck
+    {
+        public override void eat()
+        {
+            Console.WriteLine("i am eting food...");
+        }
+    }
+}
